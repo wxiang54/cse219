9b23318dbc2b3ac559b4fc72c07deecde20eec55
will <willxiang1999@gmail.com>
2018-02-21 13:02:35 -0500

    Finished Recitation4

--- RedBoxMan_HTML5/public_html/js/RedBoxManRenderer.js
+++ RedBoxMan_HTML5/public_html/js/RedBoxManRenderer.js
@@ -24,27 +24,27 @@ function init() {
     // GET THE CANVAS SO WE CAN USE IT WHEN WE LIKE
     canvas = document.getElementById("red_box_man_canvas");
     gc = canvas.getContext("2d");
-        
+
     // MAKE SURE THE CANVAS DIMENSIONS ARE 1:1 FOR RENDERING
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
     canvasWidth = canvas.width;
     canvasHeight = canvas.height;
-    
-        // FOR RENDERING TEXT
-    gc.font="32pt Arial";
+
+    // FOR RENDERING TEXT
+    gc.font = "32pt Arial";
 
     // LOAD THE RED BOX MAN IMAGE SO WE CAN RENDER
     // IT TO THE CANVAS WHENEVER WE LIKE
     redBoxManImage = new Image();
-    redBoxManImage.onload = function() {
-	imageLoaded = true;
+    redBoxManImage.onload = function () {
+        imageLoaded = true;
     }
     redBoxManImage.src = "./images/RedBoxMan.png";
-    
+
     // BY DEFAULT WE'LL START WITH MOUSE POSITION RENDERING ON
     mousePositionRendering = true;
-    
+
     // HERE'S WHERE WE'LL PUT OUR RENDERING COORDINATES
     imagesRedBoxManLocations = new Array();
     shapesRedBoxManLocations = new Array();
@@ -54,17 +54,15 @@ function processMouseClick(event) {
     updateMousePosition(event);
     var location = new Location(mouseX, mouseY);
     if (event.shiftKey) {
-	shapesRedBoxManLocations.push(location);
-	render();
-    }
-    else if (event.ctrlKey) {
-	if (imageLoaded) {
-	    imagesRedBoxManLocations.push(location);
-	    render();
-	}
-    }
-    else {
-	clear();
+        shapesRedBoxManLocations.push(location);
+        render();
+    } else if (event.ctrlKey) {
+        if (imageLoaded) {
+            imagesRedBoxManLocations.push(location);
+            render();
+        }
+    } else {
+        clear();
     }
 }
 
@@ -90,7 +88,7 @@ function renderShapesRedBoxMan(location) {
     var outlineColor = "#000000";
     var headW = 115;
     var headH = 88;
-    
+
     // DRAW HIS RED HEAD
     gc.fillStyle = headColor;
     gc.fillRect(location.x, location.y, headW, headH);
@@ -99,8 +97,73 @@ function renderShapesRedBoxMan(location) {
     gc.lineWidth = 1;
     gc.rect(location.x, location.y, headW, headH);
     gc.stroke();
-    
+
     // AND THEN DRAW THE REST OF HIM
+    var headColor =     "#DD0000";
+    var outlineColor =  "#000000";
+    var eyeColor =      "#FFFF00";
+    var pupilColor =    "#000000";
+    var mouthColor =    "#000000";
+    var bodyColor =     "#000000";
+    var headW = 115;
+    var headH = 88;
+    var eyeW = 35;
+    var eyeH = 25;
+    var eye1offset = [10, 10];
+    var eye2offset = [70, 10];
+    var pupilW = 10;
+    var pupilH = 10;
+    var pupiloffset = [10, 10];
+    var mouthW = 100;
+    var mouthH = 10;
+    var mouthoffset = [30, 50];
+    var chestW = 70;
+    var chestH = 40;
+    var chestoffset = [30, 85];
+    var bodyW = 60;
+    var bodyH = 20;
+    var bodyoffset = [35, 125];
+    var footW = 10;
+    var footH = 20;
+    var foot1offset = [30, 145];
+    var foot2offset = [90, 145];
+
+    // AND THEN DRAW THE REST OF HIM
+    //eye 1
+    gc.fillStyle = eyeColor;
+    gc.fillRect(location.x + eye1offset[0], location.y + eye1offset[1], eyeW, eyeH);
+    gc.beginPath();
+    gc.rect(location.x + eye1offset[0], location.y + eye1offset[1], eyeW, eyeH);
+    gc.stroke();
+    //eye2
+    gc.fillRect(location.x + eye2offset[0], location.y + eye2offset[1], eyeW, eyeH);
+    gc.beginPath();
+    gc.rect(location.x + eye2offset[0], location.y + eye2offset[1], eyeW, eyeH);
+    gc.stroke();
+    //pupil1
+    gc.fillStyle = pupilColor;
+    gc.fillRect(location.x + eye1offset[0] + pupiloffset[0],
+            location.y + eye1offset[1] + pupiloffset[1], pupilW, pupilH);
+    gc.beginPath();
+    //pupil2
+    gc.fillRect(location.x + eye2offset[0] + pupiloffset[0],
+            location.y + eye2offset[1] + pupiloffset[1], pupilW, pupilH);
+    gc.beginPath();
+    //mouth
+    gc.fillRect(location.x + mouthoffset[0], location.y + mouthoffset[1], mouthW, mouthH);
+    gc.beginPath();
+    //chest
+    gc.fillRect(location.x + chestoffset[0], location.y + chestoffset[1], chestW, chestH);
+    gc.beginPath();
+    //body
+    gc.fillRect(location.x + bodyoffset[0], location.y + bodyoffset[1], bodyW, bodyH);
+    gc.beginPath();
+    //foot1
+    gc.fillRect(location.x + foot1offset[0], location.y + foot1offset[1], footW, footH);
+    gc.beginPath();
+    //foot2
+    gc.fillRect(location.x + foot2offset[0], location.y + foot2offset[1], footW, footH);
+    gc.beginPath();
 }
 
 function renderImageRedBoxMan(location) {
@@ -109,7 +172,7 @@ function renderImageRedBoxMan(location) {
 
 function renderMousePositionInCanvas(event) {
     if (mousePositionRendering) {
-	gc.strokeText("(" + mouseX + "," + mouseY + ")", 10, 50);    
+        gc.strokeText("(" + mouseX + "," + mouseY + ")", 10, 50);
     }
 }
 
@@ -120,12 +183,12 @@ function toggleMousePositionRendering() {
 function render() {
     clearCanvas();
     for (var i = 0; i < shapesRedBoxManLocations.length; i++) {
-	var location = shapesRedBoxManLocations[i];
-	renderShapesRedBoxMan(location);
+        var location = shapesRedBoxManLocations[i];
+        renderShapesRedBoxMan(location);
     }
     for (var j = 0; j < imagesRedBoxManLocations.length; j++) {
-	var location = imagesRedBoxManLocations[j];
-	renderImageRedBoxMan(location);
+        var location = imagesRedBoxManLocations[j];
+        renderImageRedBoxMan(location);
     }
     renderMousePositionInCanvas();
 }
--- RedBoxMan_JavaFX/src/redboxman_javafx/RedBoxManRenderer.java
+++ RedBoxMan_JavaFX/src/redboxman_javafx/RedBoxManRenderer.java
@@ -88,10 +88,34 @@ public class RedBoxManRenderer extends Application {
     }
     
     public void renderShapeRedBoxMan(Point2D location) {
-	String headColor = "#DD0000";
-	String outlineColor = "#000000";
+	String headColor =      "#DD0000";
+	String outlineColor =   "#000000";
+        String eyeColor =       "#FFFF00";
+        String pupilColor =     "#000000";
+        String mouthColor =     "#000000";
+        String bodyColor =      "#000000";
 	int headW = 115;
 	int headH = 88;
+        int eyeW = 35;
+        int eyeH = 25;
+        int[] eye1offset = new int[]{10, 10};
+        int[] eye2offset = new int[]{70, 10};
+        int pupilW = 10;
+        int pupilH = 10;
+        int[] pupiloffset = new int[]{10, 10};
+        int mouthW = 100;
+        int mouthH = 10;
+        int[] mouthoffset = new int[]{30, 50};
+        int chestW = 70;
+        int chestH = 40;
+        int[] chestoffset = new int[]{30, 85};
+        int bodyW = 60;
+        int bodyH = 20;
+        int[] bodyoffset = new int[]{35, 125};
+        int footW = 10;
+        int footH = 20;
+        int[] foot1offset = new int[]{30, 145};
+        int[] foot2offset = new int[]{90, 145};
     
 	// DRAW HIS RED HEAD
         gc.setFill(Paint.valueOf(headColor));
@@ -103,6 +127,52 @@ public class RedBoxManRenderer extends Application {
 	gc.stroke();
 	
 	// AND THEN DRAW THE REST OF HIM
+        //eye 1
+        gc.setFill(Paint.valueOf(eyeColor));
+        gc.fillRect(location.getX()+eye1offset[0], location.getY()+eye1offset[1], eyeW, eyeH);
+        gc.beginPath();
+	gc.setStroke(Paint.valueOf(outlineColor));
+	gc.setLineWidth(1);
+	gc.rect(location.getX()+eye1offset[0], location.getY()+eye1offset[1], eyeW, eyeH);
+	gc.stroke();
+        //eye2
+        gc.setFill(Paint.valueOf(eyeColor));
+        gc.fillRect(location.getX()+eye2offset[0], location.getY()+eye2offset[1], eyeW, eyeH);
+        gc.beginPath();
+	gc.setStroke(Paint.valueOf(outlineColor));
+	gc.setLineWidth(1);
+	gc.rect(location.getX()+eye2offset[0], location.getY()+eye2offset[1], eyeW, eyeH);
+	gc.stroke();
+        //pupil1
+        gc.setFill(Paint.valueOf(pupilColor));
+        gc.fillRect(location.getX()+eye1offset[0]+pupiloffset[0], 
+                location.getY()+eye1offset[1]+pupiloffset[1], pupilW, pupilH);
+        gc.beginPath();
+        //pupil2
+        gc.setFill(Paint.valueOf(pupilColor));
+        gc.fillRect(location.getX()+eye2offset[0]+pupiloffset[0], 
+                location.getY()+eye2offset[1]+pupiloffset[1], pupilW, pupilH);
+        gc.beginPath();
+        //mouth
+        gc.setFill(Paint.valueOf(mouthColor));
+        gc.fillRect(location.getX()+mouthoffset[0], location.getY()+mouthoffset[1], mouthW, mouthH);
+        gc.beginPath();
+        //chest
+        gc.setFill(Paint.valueOf(bodyColor));
+        gc.fillRect(location.getX()+chestoffset[0], location.getY()+chestoffset[1], chestW, chestH);
+        gc.beginPath();
+        //body
+        gc.setFill(Paint.valueOf(bodyColor));
+        gc.fillRect(location.getX()+bodyoffset[0], location.getY()+bodyoffset[1], bodyW, bodyH);
+        gc.beginPath();
+        //foot1
+        gc.setFill(Paint.valueOf(bodyColor));
+        gc.fillRect(location.getX()+foot1offset[0], location.getY()+foot1offset[1], footW, footH);
+        gc.beginPath();
+        //foot2
+        gc.setFill(Paint.valueOf(bodyColor));
+        gc.fillRect(location.getX()+foot2offset[0], location.getY()+foot2offset[1], footW, footH);
+        gc.beginPath();
     }
     
     public void renderImageRedBoxMan(Point2D location) {
