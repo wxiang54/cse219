802c480db34e1575efa3da04d10bb6f389e09543
will <willxiang1999@gmail.com>
2018-02-27 12:11:09 -0500

    hw2 init

---
+++ hw2/data-vilij/src/dataprocessors/AppData.java
@@ -0,0 +1,70 @@
+package dataprocessors;
+
+import settings.AppPropertyTypes;
+import ui.AppUI;
+import vilij.components.DataComponent;
+import vilij.components.Dialog;
+import vilij.components.ErrorDialog;
+import vilij.propertymanager.PropertyManager;
+import vilij.settings.PropertyTypes;
+import vilij.templates.ApplicationTemplate;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * This is the concrete application-specific implementation of the data component defined by the Vilij framework.
+ *
+ * @author Ritwik Banerjee
+ * @see DataComponent
+ */
+public class AppData implements DataComponent {
+
+    private TSDProcessor        processor;
+    private ApplicationTemplate applicationTemplate;
+
+    public AppData(ApplicationTemplate applicationTemplate) {
+        this.processor = new TSDProcessor();
+        this.applicationTemplate = applicationTemplate;
+    }
+
+    @Override
+    public void loadData(Path dataFilePath) {
+        // TODO: NOT A PART OF HW 1
+    }
+
+    public void loadData(String dataString) {
+        try {
+            processor.processString(dataString);
+        } catch (Exception e) {
+            ErrorDialog     dialog   = (ErrorDialog) applicationTemplate.getDialog(Dialog.DialogType.ERROR);
+            PropertyManager manager  = applicationTemplate.manager;
+            String          errTitle = manager.getPropertyValue(PropertyTypes.LOAD_ERROR_TITLE.name());
+            String          errMsg   = manager.getPropertyValue(PropertyTypes.LOAD_ERROR_MSG.name());
+            String          errInput = manager.getPropertyValue(AppPropertyTypes.TEXT_AREA.name());
+            dialog.show(errTitle, errMsg + errInput);
+        }
+    }
+
+    @Override
+    public void saveData(Path dataFilePath) {
+        // NOTE: completing this method was not a part of HW 1. You may have implemented file saving from the
+        // confirmation dialog elsewhere in a different way.
+        try (PrintWriter writer = new PrintWriter(Files.newOutputStream(dataFilePath))) {
+            writer.write(((AppUI) applicationTemplate.getUIComponent()).getCurrentText());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void clear() {
+        processor.clear();
+    }
+
+    public void displayData() {
+        processor.toChartData(((AppUI) applicationTemplate.getUIComponent()).getChart());
+    }
+}
---
+++ hw2/data-vilij/src/ui/AppUI.java
@@ -0,0 +1,174 @@
+package ui;
+
+import actions.AppActions;
+import dataprocessors.AppData;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import settings.AppPropertyTypes;
+import vilij.propertymanager.PropertyManager;
+import vilij.templates.ApplicationTemplate;
+import vilij.templates.UITemplate;
+
+import static java.io.File.separator;
+import static vilij.settings.PropertyTypes.GUI_RESOURCE_PATH;
+import static vilij.settings.PropertyTypes.ICONS_RESOURCE_PATH;
+
+/**
+ * This is the application's user interface implementation.
+ *
+ * @author Ritwik Banerjee
+ */
+public final class AppUI extends UITemplate {
+
+    /** The application to which this class of actions belongs. */
+    ApplicationTemplate applicationTemplate;
+
+    @SuppressWarnings("FieldCanBeLocal")
+    private Button                       scrnshotButton; // toolbar button to take a screenshot of the data
+    private ScatterChart<Number, Number> chart;          // the chart where data will be displayed
+    private Button                       displayButton;  // workspace button to display data on the chart
+    private TextArea                     textArea;       // text area for new data input
+    private boolean                      hasNewText;     // whether or not the text area has any new data since last display
+
+    public ScatterChart<Number, Number> getChart() { return chart; }
+
+    public AppUI(Stage primaryStage, ApplicationTemplate applicationTemplate) {
+        super(primaryStage, applicationTemplate);
+        this.applicationTemplate = applicationTemplate;
+    }
+
+    @Override
+    protected void setResourcePaths(ApplicationTemplate applicationTemplate) {
+        super.setResourcePaths(applicationTemplate);
+    }
+
+    @Override
+    protected void setToolBar(ApplicationTemplate applicationTemplate) {
+        super.setToolBar(applicationTemplate);
+        PropertyManager manager = applicationTemplate.manager;
+        String iconsPath = "/" + String.join(separator,
+                                             manager.getPropertyValue(GUI_RESOURCE_PATH.name()),
+                                             manager.getPropertyValue(ICONS_RESOURCE_PATH.name()));
+        String scrnshoticonPath = String.join(separator,
+                                              iconsPath,
+                                              manager.getPropertyValue(AppPropertyTypes.SCREENSHOT_ICON.name()));
+        scrnshotButton = setToolbarButton(scrnshoticonPath,
+                                          manager.getPropertyValue(AppPropertyTypes.SCREENSHOT_TOOLTIP.name()),
+                                          true);
+        toolBar.getItems().add(scrnshotButton);
+    }
+
+    @Override
+    protected void setToolbarHandlers(ApplicationTemplate applicationTemplate) {
+        applicationTemplate.setActionComponent(new AppActions(applicationTemplate));
+        newButton.setOnAction(e -> applicationTemplate.getActionComponent().handleNewRequest());
+        saveButton.setOnAction(e -> applicationTemplate.getActionComponent().handleSaveRequest());
+        loadButton.setOnAction(e -> applicationTemplate.getActionComponent().handleLoadRequest());
+        exitButton.setOnAction(e -> applicationTemplate.getActionComponent().handleExitRequest());
+        printButton.setOnAction(e -> applicationTemplate.getActionComponent().handlePrintRequest());
+    }
+
+    @Override
+    public void initialize() {
+        layout();
+        setWorkspaceActions();
+    }
+
+    @Override
+    public void clear() {
+        textArea.clear();
+        chart.getData().clear();
+    }
+
+    public String getCurrentText() { return textArea.getText(); }
+
+    private void layout() {
+        PropertyManager manager = applicationTemplate.manager;
+        NumberAxis      xAxis   = new NumberAxis();
+        NumberAxis      yAxis   = new NumberAxis();
+        chart = new ScatterChart<>(xAxis, yAxis);
+        chart.setTitle(manager.getPropertyValue(AppPropertyTypes.CHART_TITLE.name()));
+
+        VBox leftPanel = new VBox(8);
+        leftPanel.setAlignment(Pos.TOP_CENTER);
+        leftPanel.setPadding(new Insets(10));
+
+        VBox.setVgrow(leftPanel, Priority.ALWAYS);
+        leftPanel.setMaxSize(windowWidth * 0.29, windowHeight * 0.3);
+        leftPanel.setMinSize(windowWidth * 0.29, windowHeight * 0.3);
+
+        Text   leftPanelTitle = new Text(manager.getPropertyValue(AppPropertyTypes.LEFT_PANE_TITLE.name()));
+        String fontname       = manager.getPropertyValue(AppPropertyTypes.LEFT_PANE_TITLEFONT.name());
+        Double fontsize       = Double.parseDouble(manager.getPropertyValue(AppPropertyTypes.LEFT_PANE_TITLESIZE.name()));
+        leftPanelTitle.setFont(Font.font(fontname, fontsize));
+
+        textArea = new TextArea();
+
+        HBox processButtonsBox = new HBox();
+        displayButton = new Button(manager.getPropertyValue(AppPropertyTypes.DISPLAY_BUTTON_TEXT.name()));
+        HBox.setHgrow(processButtonsBox, Priority.ALWAYS);
+        processButtonsBox.getChildren().add(displayButton);
+
+        leftPanel.getChildren().addAll(leftPanelTitle, textArea, processButtonsBox);
+
+        StackPane rightPanel = new StackPane(chart);
+        rightPanel.setMaxSize(windowWidth * 0.69, windowHeight * 0.69);
+        rightPanel.setMinSize(windowWidth * 0.69, windowHeight * 0.69);
+        StackPane.setAlignment(rightPanel, Pos.CENTER);
+
+        workspace = new HBox(leftPanel, rightPanel);
+        HBox.setHgrow(workspace, Priority.ALWAYS);
+
+        appPane.getChildren().add(workspace);
+        VBox.setVgrow(appPane, Priority.ALWAYS);
+    }
+
+    private void setWorkspaceActions() {
+        setTextAreaActions();
+        setDisplayButtonActions();
+    }
+
+    private void setTextAreaActions() {
+        textArea.textProperty().addListener((observable, oldValue, newValue) -> {
+            try {
+                if (!newValue.equals(oldValue)) {
+                    ((AppActions) applicationTemplate.getActionComponent()).setIsUnsavedProperty(true);
+                    if (newValue.charAt(newValue.length() - 1) == '\n' || newValue.isEmpty())
+                        hasNewText = true;
+                    newButton.setDisable(false);
+                    saveButton.setDisable(false);
+                }
+            } catch (IndexOutOfBoundsException e) {
+                System.err.println(newValue);
+            }
+        });
+    }
+
+    private void setDisplayButtonActions() {
+        displayButton.setOnAction(event -> {
+            if (hasNewText) {
+                try {
+                    chart.getData().clear();
+                    AppData dataComponent = (AppData) applicationTemplate.getDataComponent();
+                    dataComponent.clear();
+                    dataComponent.loadData(textArea.getText());
+                    dataComponent.displayData();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+}
